3f354e692401339d5a56430b30a78dc9ff5e4022
jongjam <jongjam64@gmail.com>
2020-04-16 23:04:56 -0700

    createTranslator method and translators

---
+++ app/src/test/java/com/jhonghawan/wordpigeon/ClassifyTest.java
@@ -0,0 +1,23 @@
+package com.jhonghawan.wordpigeon;
+
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ClassifyTest {
+
+    @Test
+    public void createTranslatorTest() {
+        FirebaseTranslatorOptions options =
+                new FirebaseTranslatorOptions.Builder()
+                        .setSourceLanguage(FirebaseTranslateLanguage.EN)
+                        .setTargetLanguage(FirebaseTranslateLanguage.EO)
+                        .build();
+
+        Classify input = new Classify(); //english is 11, eo is 12
+        assertEquals(options, input.createTranslator(11,12));
+    }
+
+}
